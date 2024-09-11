34c58bc36ce2e099499d325bf004da908c583074
perry-C <perry.chen.pc@gmail.com>
2024-09-11 14:25:49 +0300

    Feat: css improvement on chat dialog displays

--- components/MessageWindow.tsx
+++ components/MessageWindow.tsx
@@ -1,19 +1,29 @@
 import { socket } from '@/socket';
-import { Button, TextField } from '@radix-ui/themes';
+import { TextField } from '@radix-ui/themes';
+import classnames from 'classnames';
 import { ChangeEvent, useRef, useState } from 'react';
-
 interface MessageData {
     content: string;
     fromSelf: boolean;
 }
 
+const mockData = [
+    { content: 'hi chen', fromSelf: true },
+    { content: 'hi pai', fromSelf: false },
+    { content: 'what the fuck', fromSelf: false },
+    { content: 'I dont know', fromSelf: true },
+    { content: 'doesnt matter', fromSelf: false },
+    { content: 'bye ', fromSelf: true },
+    { content: 'bye ', fromSelf: false },
+];
+
 const MessageWindow = (props: any) => {
     const message = useRef('');
+
+    // const [messageLog, setMessageLog] = useState<MessageData[]>(mockData);
     const [messageLog, setMessageLog] = useState<MessageData[]>([]);
 
     socket.on('receive_message', (message) => {
-        console.log('Client receiving message');
-        console.log(messageLog);
         setMessageLog([...messageLog, { content: message, fromSelf: false }]);
     });
 
@@ -22,8 +32,6 @@ const MessageWindow = (props: any) => {
         console.log(message.current);
     };
     const handleMessageSend = () => {
-        console.log('handle Message Send');
-
         if (message.current !== '') {
             socket.emit('send_message', {
                 message,
@@ -37,23 +45,37 @@ const MessageWindow = (props: any) => {
         }
     };
 
-    const messageListMap = messageLog.map((m) => <ul>{m.content}</ul>);
-    return (
-        <div className='h-full flex flex-col justify-end'>
-            <div id='message-display-window'>
-                <ul>{messageListMap}</ul>
+    const messageListMap = messageLog.map((m) => (
+        <li className='flex'>
+            <div
+                className={classnames({
+                    'ml-auto bg-compPri': m.fromSelf,
+                    'mr-auto bg-compSec': !m.fromSelf,
+                    'text-white text-2xl flex p-4 rounded-lg': true,
+                })}
+            >
+                {m.content}
             </div>
-            <div id='message-input-bar' className='flex space-x-2'>
+        </li>
+    ));
+    return (
+        <div className='flex flex-col justify-end space-y-4 h-full'>
+            <div id='message-display-window'>
+                <ul className='space-y-2'>{messageListMap}</ul>
+            </div>
+            <div id='message-input-bar' className='flex space-x-4 h-12'>
                 <TextField.Root
-                    className='w-full'
+                    className='w-full min-h-full'
                     variant='classic'
                     onChange={handleMessageUpdate}
+                    size='3'
+                ></TextField.Root>
+                <button
+                    onClick={handleMessageSend}
+                    className='border-compPri border p-2 text-compPri rounded-lg hover:bg-compSec hover:text-white active:bg-compPri transition'
                 >
-                    <TextField.Slot></TextField.Slot>
-                </TextField.Root>
-                <Button variant='soft' onClick={handleMessageSend}>
                     Send
-                </Button>
+                </button>
             </div>
         </div>
     );
